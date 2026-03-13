cd0d2b4e681f749e2b2daefad9c5849d5d125e8c
Artyom Sokolov <dyein@bk.ru>
2026-03-13 13:33:15 +0300

    Rewokring client

--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -4,8 +4,18 @@ from httpx import Response
 from clients.api_client import APIClient
 from clients.public_http_builder import get_public_http_client
 
+# Добавили описание структуры пользователя
+class User(TypedDict):
+    """
+    Описание структуры пользователя.
+    """
+    id: str
+    email: str
+    lastName: str
+    firstName: str
+    middleName: str
 
-class CreateRequestDict(TypedDict):
+class CreateUserRequestDict(TypedDict):
     """
     Описание структуры запроса на создание пользователя.
     """
@@ -15,18 +25,30 @@ class CreateRequestDict(TypedDict):
     firstName: str
     middleName: str
 
+# Добавили описание структуры ответа создания пользователя
+class CreateUserResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: User
+
 class PublicUsersClient(APIClient):
     """
     Клиент для работы с /api/v1/users
     """
-    def create_user_api(self, request: CreateRequestDict) -> Response:
+    def create_user_api(self, request: CreateUserRequestDict) -> Response:
         """
-        Метод выполняет аутентификацию пользователя.
+        Метод создает пользователя.
 
-        :param request: Словарь с данными создаваемого пользователя.
+        :param request: Словарь с email, password, lastName, firstName, middleName.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post("/api/v1/users", json=request)
+
+    # Добавили новый метод
+    def create_user(self, request: CreateUserRequestDict) -> CreateUserResponseDict:
+        response = self.create_user_api(request)
+        return response.json()
 
 def get_public_users_client() -> PublicUsersClient:
     """
