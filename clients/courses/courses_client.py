94d81ad83c809d61d3610f895800be711bb6c0b3
Artyom Sokolov <dyein@bk.ru>
2026-03-13 17:04:38 +0300

    WOrk

--- clients/courses/courses_client.py
+++ clients/courses/courses_client.py
@@ -1,7 +1,10 @@
 from typing import TypedDict
 from httpx import Response
+
 from clients.api_client import APIClient
+from clients.files.files_client import File
 from clients.private_http_builder import AuthenticationUserDict, get_private_http_client
+from clients.users.private_users_client import User
 
 
 class GetCoursesQueryDict(TypedDict):
@@ -9,6 +12,16 @@ class GetCoursesQueryDict(TypedDict):
     Описание структуры запроса на получение списка курсов.
     """
     userId: str
+
+class Course(TypedDict):
+    id: str
+    title: str
+    maxScore: int
+    minScore: int
+    description: str
+    estimatedTime: str
+    previewFile: File
+    createdByUser: User
 
 class CreateCourseRequestDict(TypedDict):
     """
@@ -21,6 +34,9 @@ class CreateCourseRequestDict(TypedDict):
     estimatedTime: str
     previewFileId: str
     createdByUserId: str
+
+class CreateCourseResponseDict(TypedDict):
+    course: Course
 
 class UpdateCourseRequestDict(TypedDict):
     """
@@ -82,6 +98,10 @@ class CoursesClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.delete(f"/api/v1/courses/{course_id}")
+
+    def create_course(self, request: CreateCourseRequestDict) -> CreateCourseRequestDict:
+        response = self.create_course_api(request)
+        return response.json()
 
 def get_courses_client(user: AuthenticationUserDict) -> CoursesClient:
     """
--- clients/files/files_client.py
+++ clients/files/files_client.py
@@ -4,6 +4,11 @@ from typing import TypedDict
 
 from clients.private_http_builder import AuthenticationUserDict, get_private_http_client
 
+class File(TypedDict):
+    id: str
+    filename: str
+    directory: str
+    url: str
 
 class CreateFileRequestDict(TypedDict):
     """
@@ -12,6 +17,9 @@ class CreateFileRequestDict(TypedDict):
     filename: str
     directory: str
     upload_file: str
+
+class CreateFileResponse(TypedDict):
+    file: File
 
 class FilesClient(APIClient):
     """
@@ -47,6 +55,10 @@ class FilesClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.delete(f"/api/v1/files/{file_id}")
+
+    def create_file(self, request: CreateFileRequestDict) -> CreateFileRequestDict:
+        response = self.create_file_api(request)
+        return response.json()
 
 def get_files_client(user: AuthenticationUserDict) -> FilesClient:
     """
