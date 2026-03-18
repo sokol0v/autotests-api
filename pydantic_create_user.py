fda3cdd883a2af1669f85c0fdc758e6e8f251509
Artyom Sokolov <dyein@bk.ru>
2026-03-18 15:35:03 +0300

    Знакомство с Pydantic. Практика написания Pydantic-моделей

---
+++ pydantic_create_user.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, EmailStr
+
+class UserSchema(BaseModel):
+    """
+    Описание структуры пользователя
+    """
+    id: str
+    email: EmailStr
+    lastName: str
+    firstName: str
+    middleName: str
+
+class CreateUserRequestSchema(BaseModel):
+    """
+    Описание структуры запроса на создание пользователя.
+    """
+    email: str
+    password: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+class CreateUserResponseDict(BaseModel):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: UserSchema
